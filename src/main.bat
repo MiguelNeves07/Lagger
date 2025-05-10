41e4be0f01840c2a0962aa23e1ee193711e88ff4
neves07 <109999413+MiguelNeves07@users.noreply.github.com>
2025-05-10 01:03:33 -0300

    Add files via upload

---
+++ src/main.bat
@@ -0,0 +1,101 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcDGRKch7FLwT1Mz0+OSLrUwZR95uNorD39Q=
+::fBE1pAF6MU+EWHreyHcjLQlHcDGRKch7FLwT1Mz0+OSLrUwZR95tNorD39Q=
+::fBE1pAF6MU+EWHreyHcjLQlHcDGRKch7FLwT1Mz0+OSLrUwZR95sNorD39Q=
+::fBE1pAF6MU+EWHreyHcjLQlHcDGRKch7FLwT1Mz0+OSLrUwZR95rNorD39Q=
+::fBE1pAF6MU+EWHreyHcjLQlHcDGRKch7FLwT1Mz0+OSLrUwZR95qNorD39Q=
+::fBE1pAF6MU+EWHreyHcjLQlHcDGRKch7FLwT1Mz0+OSLrUwZR95pNorD39Q=
+::fBE1pAF6MU+EWHreyHcjLQlHcDGRKch7FLwT1Mz0+OSLrUwZR95oNorD39Q=
+::fBE1pAF6MU+EWHreyHcjLQlHcDGRKch7FLwT1Mz0+OSLrUwZR95nNorD39Q=
+::fBE1pAF6MU+EWHreyHcjLQlHcDGRKch7FLwT1Mz0+OSLrUwZR95mNorD39Q=
+::fBE1pAF6MU+EWHreyHcjLQlHcDGRKch7FLwT1Mz0+OSLrUwZR95uKMHewrHu
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFC5HWadDLmK1OpET/+b34OuDsXE/W+89R6zU1bixc7VCuhCqfJUitg==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXqZg==
+::ZQ05rAF9IAHYFVzEqQIcKRxTSRbi
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQIcKRwUVQuXLiuKJfR8
+::dhA7uBVwLU+EWFeB4kcjeEwAHVfi
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFC5HWadDLmK1OpET/+b34OuDsXEQVesxNorD39Q=
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+color a
+cls
+title LAGGER 1.0
+
+echo.
+echo LAGGER - LAGGER - LAGGER - LAGGER - LAGGER
+echo - LAGGER - LAGGER - LAGGER - LAGGER - LAGGER
+echo LAGGER - LAGGER - LAGGER - LAGGER - LAGGER
+echo - LAGGER - LAGGER - LAGGER - LAGGER - LAGGER
+echo LAGGER - LAGGER - LAGGER - LAGGER - LAGGER
+echo - LAGGER - LAGGER - LAGGER - LAGGER - LAGGER
+echo LAGGER - LAGGER - LAGGER - LAGGER - LAGGER
+echo - LAGGER - LAGGER - LAGGER - LAGGER - LAGGER
+echo.
+echo 1 - Lagar PC
+echo 2 - End
+echo.
+set /p opcao= - 
+
+if %opcao% equ 1 goto lag
+if %opcao% equ 2 goto end
+if %opcao% GEQ 3 goto erro
+
+:lag
+cls
+start 1.exe
+start 2.exe
+start 3.exe
+start 4.exe
+start 5.exe
+start 6.exe
+start 7.exe
+start 8.exe
+start 9.exe
+start 10.exe
+pause
+
+:erro
+cls
+
+echo LAGGER - LAGGER - LAGGER
+echo - LAGGER - LAGGER - LAGGER -
+echo LAGGER - LAGGER - LAGGER
+echo - LAGGER - LAGGER - LAGGER -
+echo LAGGER - LAGGER - LAGGER
+echo - LAGGER - LAGGER - LAGGER -
+echo LAGGER - LAGGER - LAGGER
+echo - LAGGER - LAGGER - LAGGER -
+echo.
+color c
+echo OPS, PARECE QUE HOUVE UM ERRO!
+pause
+exit
+
+:end
+cls
+exit
